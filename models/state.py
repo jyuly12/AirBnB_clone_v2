c8516147b92906c7d7a28a52d3a27c2e3cec89a8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-10 18:38:52 -0500

    models/state.py

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -12,3 +14,15 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship("cities", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """
+        return the cities with the same state_id
+        """
+        list_cities_state_id = []
+        all_cities = models.storage.all(City)
+        for value in all_cities.values():
+            if value.state_id == self.id:
+                list_cities_state_id.append(value)
+        return list_cities_state_id
