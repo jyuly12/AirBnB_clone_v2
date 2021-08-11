33f2fc053acefc5a44c1da6c180012ddced85bf6
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-08-11 20:16:13 +0000

    Update do_all function

--- console.py
+++ console.py
@@ -210,6 +210,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
+        from os import environ
         print_list = []
 
         if args:
@@ -217,11 +218,12 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            else:
+                for k, v in storage.all().items():
+                    if k.split('.')[0] == args:
+                        print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
