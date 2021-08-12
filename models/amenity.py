f60da87b0fd56320aea11987c61071a6e8abe283
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-11 23:16:58 -0500

    models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
-    name = ""
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+
+
+class Amenity(BaseModel, Base):
+    """class amenity"""
+
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secundary="place_amenity", back_populates="children"  )
