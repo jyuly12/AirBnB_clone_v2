1460881c55ef1613ef4c7e56204cf0b52f1902e0
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-09-15 23:28:49 -0500

    Add new instances

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Defines the function do_deploy
+"""
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """
+        deserializing the JSON file to objects
+        """
+        self.reload()
